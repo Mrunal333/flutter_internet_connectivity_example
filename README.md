75859a2c200b8e6c15b25bd6596637e230793775
Mrunal333 <mrunalsheramkar333@2gmail.com>
2022-07-12 18:21:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# flutter_internet_connectivity_example
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+## 📷 Screenshots
+
+<table>
+  <tr>
+    <td align="center">
+      <img src="https://raw.githubusercontent.com/jogboms/flutter_offline/master/screenshots/demo_1.gif" width="250px">
+    </td>
+    <td align="center">
+      <img src="https://raw.githubusercontent.com/jogboms/flutter_offline/master/screenshots/demo_2.gif" width="250px">
+    </td>
+    <td align="center">
+      <img src="https://raw.githubusercontent.com/jogboms/flutter_offline/master/screenshots/demo_3.gif" width="250px">
+    </td>
+  </tr>
+</table>
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import './widgets/demo_1.dart';
+import './widgets/demo_2.dart';
+import './widgets/demo_3.dart';
+import 'widgets/demo_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Offline Demo',
+      theme: ThemeData.dark(),
+      home: Builder(
+        builder: (BuildContext context) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  navigate(context, const Demo1());
+                },
+                child: const Text('Demo 1'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  navigate(context, const Demo2());
+                },
+                child: const Text('Demo 2'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  navigate(context, const Demo3());
+                },
+                child: const Text('Demo 3'),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  void navigate(BuildContext context, Widget widget) {
+    Navigator.of(context).push<void>(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) => DemoPage(child: widget),
+      ),
+    );
+  }
+}
